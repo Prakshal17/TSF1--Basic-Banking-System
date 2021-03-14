3ad0748f06e490916fca425af0baf9dbf5611689
PRAKSHAL JAIN <54144239+Prakshal17@users.noreply.github.com>
2021-03-14 17:01:07 +0530

    Update user_script.php

--- user_script.php
+++ user_script.php
@@ -3,7 +3,7 @@ require 'includes/common.php';
 $name=mysqli_real_escape_string($con,$_POST['name']);
 $email=mysqli_real_escape_string($con,$_POST['email']);
 $balance=mysqli_real_escape_string($con,$_POST['balance']);
-$insert="insert into users(name,email,balance) values('$name','$email','$balance')";
+$insert="insert into customers(name,email,balance) values('$name','$email','$balance')";
 $insert_query=mysqli_query($con,$insert);
-header('location: transaction.php');
+header('location: customers.php');
 ?>
